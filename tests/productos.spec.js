5a78da8883da38cbe0d55a20fc815213405f4592
desotvh <berendicto@gmail.com>
2024-02-12 18:34:49 -0300

    cambios en las exportaciones desde index.js

--- tests/productos.spec.js
+++ tests/productos.spec.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const server = require("../index");
+const app = require("../index");
 
 describe("Operaciones CRUD", () => {
   // creo una variable para almacenar el id de cafe
@@ -7,7 +7,7 @@ describe("Operaciones CRUD", () => {
 
   // punto 1
   it("Testea que la ruta GET /cafes devuelve un status code 200 y el tipo de dato recibido es un arreglo con por lo menos 1 objeto", async () => {
-    const response = await request(server).get("/cafes");
+    const response = await request(app).get("/cafes");
     expect(response.status).toBe(200);
     expect(Array.isArray(response.body)).toBe(true);
     expect(response.body.length).toBeGreaterThan(0);
@@ -16,7 +16,7 @@ describe("Operaciones CRUD", () => {
   // Punto 2
   it("Comprueba que se obtiene un código 404 al intentar eliminar un café con un id que no existe o un código 400 cuando falta el token de autorización", async () => {
     const cafeIdNoExistente = "id_que_no_existe";
-    const response = await request(server).delete(`/cafes/${cafeIdNoExistente}`);
+    const response = await request(app).delete(`/cafes/${cafeIdNoExistente}`);
     // esto se hizo para verificar si el codigo es 404 cuando no existe el cafe y 400 cuando se requiere de un token
     expect(response.status === 404 || response.status === 400).toBe(true);
   });
@@ -30,7 +30,7 @@ describe("Operaciones CRUD", () => {
       precio: 2.5,
     };
 
-    const response = await request(server).post("/cafes").send(nuevoCafe);
+    const response = await request(app).post("/cafes").send(nuevoCafe);
     expect(response.status).toBe(201);
     expect(response.body).toEqual(expect.arrayContaining([expect.objectContaining(nuevoCafe)]));
 
@@ -47,7 +47,7 @@ describe("Operaciones CRUD", () => {
       precio: 3.0,
     };
 
-    const response = await request(server)
+    const response = await request(app)
       .put(`/cafes/${cafeId}`)
       .send(cafeActualizado);
 
