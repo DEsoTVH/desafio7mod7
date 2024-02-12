1658710382be10a61dc29f86c2a1849b1fb18b1e
desotvh <berendicto@gmail.com>
2024-02-12 17:20:05 -0300

    Primer uploadmixed2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const cafes = require("./cafes.json"); // Modifica la ruta si es necesario
+const cafes = require("./cafes.json"); 
 
 app.listen(3000, () => {
   console.log("SERVER ON");
